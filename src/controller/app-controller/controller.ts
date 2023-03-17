783437e24e0b1e081feed82e4a5d68ae784e7209
Gownipalli Yeswanth <gownipalli.yeswanth@agilepoint.com>
2023-03-17 14:26:53 +0530

    controller added

---
+++ src/controller/app-controller/controller.ts
@@ -0,0 +1,82 @@
+import { todoView } from "../../view/todo-view.js";
+
+import { localServer } from "../storage-controller/local-server.js";
+
+import { cloudServer } from "../storage-controller/cloud-server.js";
+
+// import { eventManager } from "./event-manager.js";
+
+import { ITodoObject } from "../storage-controller/type.js";
+
+let storage = document.querySelector('.storage') as HTMLSelectElement;
+const btn = document.querySelector('.btn') as HTMLButtonElement;
+export const input = document.querySelector('.input') as HTMLInputElement;
+export const ul = document.querySelector('.taskList') as HTMLUListElement;
+
+
+
+export const selectStorage = (storage.value === 'localStorage')? cloudServer : localServer;
+function control() {
+    return {
+        createAllTasks: async function () {
+            if (storage.value === 'cloudStorage') {
+                let list = await cloudServer().getAllItems()
+                list.map(({ name , isCompleted , id} : ITodoObject) => {
+                    this.instance({name, isCompleted, id})
+                })
+                
+            } else {
+                let todo = localServer().getAllItems()
+                todo.map(({ name  , isCompleted, id} : ITodoObject) => {
+                    this.instance( {name, isCompleted , id })
+                    console.log(id)
+                }) 
+            }
+        },
+
+        createSingleTask: async function () {
+            const value = input.value
+            if (value) {
+                input.value = '';
+                let result =await selectStorage().postSingleItem(value) 
+                if (result.id && result.name) {
+                    let postArguments = { name : result.name , id : result.id , isCompleted : result.isCompleted}
+                    this.instance( postArguments )
+                }
+                else {
+                    let args = { name : value , id : result.id}
+                    this.instance( args )
+                    console.log(result)
+                }
+            } else {
+                alert('Enter task name')
+            }
+        },
+
+        instance: function ({name ,id , isCompleted } : ITodoObject) {
+            return todoView().createListElement(name , id as number , isCompleted as boolean)
+        },
+    }
+}
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault()
+    control().createSingleTask()
+})
+
+storage.addEventListener('change', () => {
+   let changeStorage = confirm(`U are changing the storage .=> you data will store only in ${storage.value}`)
+
+    if(changeStorage === true){
+        ul.innerHTML = ''
+        control().createAllTasks()
+    }    
+})
+
+control().createAllTasks();
+
+(document.querySelector('.clearAllBtn') as HTMLButtonElement).addEventListener('click', () => {
+    selectStorage().deleteAllItems()
+})
+
+
--- src/view/todo-view.ts
+++ src/view/todo-view.ts
@@ -1,8 +1,8 @@
 import { createNode , appendNode } from "../utils/functions";
 
-export function todoView(todoEventManager : Function) {
+export function todoView() {
     const ul = document.querySelector('.taskList') as HTMLInputElement;
-    const { checkedOfItem, updateOfList, singleTaskDelete } = todoEventManager()
+    // const { checkedOfItem, updateOfList, singleTaskDelete } = todoEventManager()
 
     return {
 
@@ -10,16 +10,16 @@ export function todoView(todoEventManager : Function) {
             const li = createNode('li') as HTMLLIElement
             li.classList.add('li-List')
             const span = createNode('span', elem) as HTMLSpanElement
-            appendNode(li, updateInput(span, elem, index, value , checkedOfItem))
+            appendNode(li, updateInput(span, elem, index, value )) 
             appendNode(li, span)
-            appendNode(li, editButton(span, index, elem , updateOfList ))
-            appendNode(li, deleteButton(index, li , singleTaskDelete))
-            return appendNode(ul, li)
+            appendNode(li, editButton(span, index, elem ))
+            appendNode(li, deleteButton(index, li ))
+            return appendNode(ul , li)
         },
     }
 }
 
-function updateInput(span: HTMLSpanElement, elem: string, index: number, value: boolean , checkedOfItem : Function) {
+function updateInput(span: HTMLSpanElement, elem: string, index: number, value: boolean ) {
     const input = createNode('input') as HTMLInputElement
     input.setAttribute('type', 'checkbox');
     input.classList.add('checkBox');
@@ -27,19 +27,19 @@ function updateInput(span: HTMLSpanElement, elem: string, index: number, value: 
         input.checked = true;
         span.style.textDecoration = 'line-through'
     }
-    input.addEventListener('click', ()=> checkedOfItem( input, span, elem, index));
+    // input.addEventListener('click', ()=> checkedOfItem( input, span, elem, index));
     return input;
 }
 
-function editButton(span: HTMLSpanElement, index: number, elem: string , updateOfList : Function) {
+function editButton(span: HTMLSpanElement, index: number, elem: string) {
     let editBtn = createNode('button', `<i class="fas fa-pencil"></i>`);
-    editBtn.addEventListener('click',()=> updateOfList(span, index, elem, editBtn));
+    // editBtn.addEventListener('click',()=> updateOfList(span, index, elem, editBtn));
     return editBtn;
 }
 
-function deleteButton(index: number, li: HTMLLIElement , singleTaskDelete : Function) {
+function deleteButton(index: number, li: HTMLLIElement ) {
     const button = createNode('button', `<i class="fa-solid fa-xmark"></i>`)
-    button.addEventListener('click',()=> singleTaskDelete( index, li))
+    // button.addEventListener('click',()=> singleTaskDelete( index, li))
     return button
 }
 
